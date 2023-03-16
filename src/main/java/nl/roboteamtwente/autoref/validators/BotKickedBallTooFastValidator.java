a18c56a29f836c8afe1f9d48b021c70d6b405b09
jobvd <j.j.vandieten@student.utwente.nl>
2023-03-16 12:41:28 +0100

    Changed speed to m/s, added other if-statement to see which works more reliably

--- src/main/java/nl/roboteamtwente/autoref/validators/BotKickedBallTooFastValidator.java
+++ src/main/java/nl/roboteamtwente/autoref/validators/BotKickedBallTooFastValidator.java
@@ -12,23 +12,40 @@ import java.util.EnumSet;
 public class BotKickedBallTooFastValidator implements RuleValidator{
     @Override
     public RuleViolation validate(Game game) {
-        // TODO: Currently not using Chipped variable, only 2d coordinates
         TeamColor team;
         int byBot;
         Vector2 location;
         Ball ball = game.getBall();
-        boolean chipped = false;
 
-        // Position between the previous frame and current frame
-        float speed = game.getPrevious().getBall().getPosition().xy().distance(ball.getPosition().xy());
+//        // Speed in m/s from the previous frame
+//        float prevSpeed = game.getPrevious().getPrevious().getBall().getPosition().xy().distance(game.getPrevious().getBall().getPosition().xy()) * 80;
 
+        // Speed in m/s
+        float speed = game.getPrevious().getBall().getPosition().xy().distance(ball.getPosition().xy()) * 80;
+
+        // TODO: Test which implementation works best
         // If speed in one frame is higher than (6.5m / 80fps) = 0.08125 m / frame, bot kicked it too fast
-        if (speed > 0.08125) {
+        if (speed > 6.5) {
+            team = ball.getLastTouchedBy().getTeam().getColor();
+            byBot = ball.getLastTouchedBy().getId();
+            location = ball.getPosition().xy();
+            return new Violation(team, byBot, location, speed);
+        }
+
+        /*
+        // If ball speed is higher than 6.5m/s for two consecutive frames, send violation.
+        // Checking just a single frame may be inconsistent due to the ball potentially teleporting a little between frames
+        if (prevSpeed > 6.5 && speed > 6.5) {
             team = ball.getLastTouchedBy().getTeam().getColor();
             byBot = ball.getLastTouchedBy().getId();
             location = ball.getPosition().xy();
             return new Violation(team, byBot, location, speed, chipped);
         }
+
+
+         */
+
+
 
         return null;
     }
@@ -39,10 +56,10 @@ public class BotKickedBallTooFastValidator implements RuleValidator{
         return EnumSet.of(GameState.KICKOFF, GameState.DIRECT_FREE, GameState.INDIRECT_FREE, GameState.RUNNING);
     }
 
-    record Violation(TeamColor byTeam, int byBot, Vector2 location, float speed, boolean chipped) implements RuleViolation {
+    record Violation(TeamColor byTeam, int byBot, Vector2 location, float speed) implements RuleViolation {
         @Override
         public String toString() {
-            return "Bot kicked ball too fast (by: " + byTeam + ", by bot #" + byBot + ", at " + location + ", ball speed: " + speed + ", chipped: " + chipped + ")";
+            return "Bot kicked ball too fast (by: " + byTeam + ", by bot #" + byBot + ", at " + location + ", ball speed: " + speed + ")";
         }
 
         @Override
@@ -53,7 +70,6 @@ public class BotKickedBallTooFastValidator implements RuleValidator{
                             .setByTeam(byTeam == TeamColor.BLUE ? SslGcCommon.Team.BLUE : SslGcCommon.Team.YELLOW)
                             .setByBot(byBot)
                             .setLocation(SslGcGeometry.Vector2.newBuilder().setX(location.getX()).setY(location.getY()))
-                            .setChipped(chipped)
                             .setInitialBallSpeed(speed))
                     .build();
         }
