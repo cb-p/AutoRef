6063722da2a2913c898789fadb0aa5fdc5053a0e
Minh Vo <108131130+vnminh0204@users.noreply.github.com>
2023-03-21 11:27:24 +0100

    finish DefenderInDefenseAreaValidator - Moh

--- src/main/java/nl/roboteamtwente/autoref/Referee.java
+++ src/main/java/nl/roboteamtwente/autoref/Referee.java
@@ -9,11 +9,11 @@ import java.util.List;
 public class Referee {
     private static final List<RuleValidator> RULE_VALIDATORS = List.of(
             new BotCrashingValidator(),
-            new BotTooFastInStopValidator()
+            new BotTooFastInStopValidator(),
 //            new AttackerTouchedBallInDefenseAreaValidator(),
 //            new BallLeftFieldTouchLineValidator(),
 //            new BallLeftFieldGoalLineValidator(),
-//            new DefenderInDefenseAreaValidator()
+            new DefenderInDefenseAreaValidator()
     );
 
     private List<RuleValidator> activeValidators;
