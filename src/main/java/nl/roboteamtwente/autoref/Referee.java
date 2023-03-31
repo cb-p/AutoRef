5b2384648262e110be99c6e652291f71561ceeda
Minh Vo <108131130+vnminh0204@users.noreply.github.com>
2023-03-31 13:10:57 +0200

    finish stub implementation

--- src/main/java/nl/roboteamtwente/autoref/Referee.java
+++ src/main/java/nl/roboteamtwente/autoref/Referee.java
@@ -7,14 +7,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Referee {
-    private static final List<RuleValidator> RULE_VALIDATORS = List.of(
-            new BotCrashingValidator(),
+    private static final List<RuleValidator> NON_STOPPING_RULE_VALIDATORS = List.of(
+        // Non-stopping rules
+        new BotCrashingValidator(),
+        new BotKickedBallTooFastValidator(),
+        new AttackerTouchedBallInDefenseAreaValidator()
+    );
+
+    private static final PossibleGoalValidator POSSIBLE_GOAL_VALIDATOR = new PossibleGoalValidator();
+    private static final List<RuleValidator> OTHER_RULE_VALIDATORS = List.of(
             new BotInterferedPlacementValidator(),
             new BotTooFastInStopValidator(),
-            new AttackerTouchedBallInDefenseAreaValidator(),
+//            new PossibleGoalValidator(),
             new BallLeftFieldTouchLineValidator(),
             new BallLeftFieldGoalLineValidator(),
-            new BotKickedBallTooFastValidator(),
             new DefenderInDefenseAreaValidator(),
             new AttackerDoubleTouchedBallValidator(),
             new AimlessKickValidator(),
@@ -25,6 +31,7 @@ public class Referee {
 
     private List<RuleValidator> activeValidators;
 
+    private List<RuleValidator> activeNonStoppingFouls;
     private Game game;
 
     public Game getGame() {
@@ -37,7 +44,7 @@ public class Referee {
 
     public List<RuleViolation> validate() {
         if (activeValidators == null || game.getState() != game.getPrevious().getState()) {
-            List<RuleValidator> validators = RULE_VALIDATORS.stream().filter((validator) -> validator.isActive(game)).toList();
+            List<RuleValidator> validators = OTHER_RULE_VALIDATORS.stream().filter((validator) -> validator.isActive(game)).toList();
 
             List<RuleValidator> toReset = new ArrayList<>(validators);
             if (activeValidators != null) {
@@ -48,7 +55,52 @@ public class Referee {
             activeValidators = validators;
         }
 
+        if (activeNonStoppingFouls == null || game.getState() != game.getPrevious().getState()) {
+            List<RuleValidator> validators = NON_STOPPING_RULE_VALIDATORS.stream().filter((validator) -> validator.isActive(game)).toList();
+
+            List<RuleValidator> toReset = new ArrayList<>(validators);
+            if (activeNonStoppingFouls != null) {
+                toReset.removeAll(activeNonStoppingFouls);
+            }
+
+            toReset.forEach(validator -> validator.reset(game));
+            activeNonStoppingFouls = validators;
+        }
+
         List<RuleViolation> violations = new ArrayList<>();
+
+        // First loop through all non-stopping validator
+        for (RuleValidator nonStoppingValidator : activeNonStoppingFouls) {
+            try {
+                RuleViolation violation = nonStoppingValidator.validate(game);
+                if (violation != null) {
+                    // Check for non-stopping fouls
+                    POSSIBLE_GOAL_VALIDATOR.setLastNonStoppingFoul(game.getTime());
+                    violations.add(violation);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.err.println("!! " + nonStoppingValidator.getClass().getSimpleName() + " will now be deactivated.");
+                activeNonStoppingFouls = new ArrayList<>(activeNonStoppingFouls);
+                activeNonStoppingFouls.remove(nonStoppingValidator);
+            }
+        }
+
+        // Then check possible goal
+        if (POSSIBLE_GOAL_VALIDATOR.isActive(game)) {
+            try {
+
+                RuleViolation violation = POSSIBLE_GOAL_VALIDATOR.validate(game);
+                if (violation != null) {
+                    // Check for non-stopping fouls
+                    violations.add(violation);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.err.println("!! Possible goal check error - Referee.java.");
+            }
+        }
+
         for (RuleValidator validator : activeValidators) {
             try {
                 RuleViolation violation = validator.validate(game);
@@ -58,12 +110,12 @@ public class Referee {
                 }
             } catch (Exception e) {
                 e.printStackTrace();
-
                 System.err.println("!! " + validator.getClass().getSimpleName() + " will now be deactivated.");
                 activeValidators = new ArrayList<>(activeValidators);
                 activeValidators.remove(validator);
             }
         }
+
         return violations;
     }
 }
---
+++ src/main/java/nl/roboteamtwente/autoref/validators/PossibleGoalValidator.java
@@ -0,0 +1,57 @@
+package nl.roboteamtwente.autoref.validators;
+
+import nl.roboteamtwente.autoref.RuleValidator;
+import nl.roboteamtwente.autoref.RuleViolation;
+import nl.roboteamtwente.autoref.model.Game;
+import nl.roboteamtwente.autoref.model.GameState;
+import nl.roboteamtwente.autoref.model.TeamColor;
+import nl.roboteamtwente.autoref.model.Vector2;
+import org.robocup.ssl.proto.SslGcCommon;
+import org.robocup.ssl.proto.SslGcGameEvent;
+import org.robocup.ssl.proto.SslGcGeometry;
+
+public class PossibleGoalValidator implements RuleValidator {
+
+    private double lastNonStoppingFoul;
+
+    public void setLastNonStoppingFoul(double lastNonStoppingFoul) {
+        this.lastNonStoppingFoul = lastNonStoppingFoul;
+    }
+
+    @Override
+    public RuleViolation validate(Game game) {
+
+//        Vector2 location =
+        return null;
+    }
+
+    @Override
+    public boolean isActive(Game game) {
+        return game.getState() == GameState.RUNNING;
+    }
+
+    record PossibleGoal(TeamColor byTeam, TeamColor kickingTeam, int kickingBot, Vector2 location, Vector2 kickLocation, Float maxBallHeight, int numRobotsByTeam, int lastTouchTimestampByTeam) implements RuleViolation {
+        @Override
+        public String toString() {
+            return "Possible goal (by: " + byTeam + ", kicking team: " + kickingTeam + ", bot #" + kickingBot + ", location:" + location + ", kick location:" + kickLocation
+                    + ", num robots: "+ numRobotsByTeam + ")";
+        }
+
+        @Override
+        public SslGcGameEvent.GameEvent toPacket() {
+            return SslGcGameEvent.GameEvent.newBuilder()
+                    .setType(SslGcGameEvent.GameEvent.Type.POSSIBLE_GOAL)
+                    .setPossibleGoal(SslGcGameEvent.GameEvent.Goal.newBuilder()
+                            .setByTeam(byTeam == TeamColor.BLUE ? SslGcCommon.Team.BLUE : SslGcCommon.Team.YELLOW)
+                            .setKickingTeam(kickingTeam == TeamColor.BLUE ? SslGcCommon.Team.BLUE : SslGcCommon.Team.YELLOW)
+                            .setKickingBot(kickingBot)
+                            .setLocation(SslGcGeometry.Vector2.newBuilder().setX(location.getX()).setY(location.getY()))
+                            .setMaxBallHeight(maxBallHeight)
+                            .setKickLocation(SslGcGeometry.Vector2.newBuilder().setX(kickLocation.getX()).setY(kickLocation.getY()))
+                            .setNumRobotsByTeam(numRobotsByTeam)
+                    )
+                    .build();
+
+        }
+    }
+}
